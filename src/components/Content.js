4ddb99788f4f2a5ce4a36b489b6258aaac457667
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-16 17:28:46 -0300

    Criado componentes Profile, ProfileEdit e NotFound

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
 import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
 import Search from '../pages/Search';
 
 export default class Content extends Component {
   render() {
     return (
-      <div>
-        <Route path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/search" component={ Search } />
-        <Route path="/search" component={ Search } />
-        <Route path="/search" component={ Search } />
-      </div>
+      <main>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
+      </main>
     );
   }
 }
