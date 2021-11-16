b37e949b6a60768103228c3052e93cc130069b19
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-16 16:55:18 -0300

    Criado componente Album

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import Album from '../pages/Album';
 import Login from '../pages/Login';
 import Search from '../pages/Search';
 
@@ -9,7 +10,7 @@ export default class Content extends Component {
       <div>
         <Route path="/" component={ Login } />
         <Route path="/search" component={ Search } />
-        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
         <Route path="/search" component={ Search } />
         <Route path="/search" component={ Search } />
         <Route path="/search" component={ Search } />
