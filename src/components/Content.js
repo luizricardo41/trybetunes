62c4a3ef7d3c2dd6050f6f5d484e2ba41c529e67
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-16 16:52:18 -0300

    Criado componente Search

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Login from '../pages/Login';
+import Search from '../pages/Search';
 
 export default class Content extends Component {
   render() {
     return (
       <div>
         <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/search" component={ Search } />
+        <Route path="/search" component={ Search } />
+        <Route path="/search" component={ Search } />
+        <Route path="/search" component={ Search } />
       </div>
     );
   }
