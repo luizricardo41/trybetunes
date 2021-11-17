5c303a07c28f824f4bdd7c84b6461aa9b4257b79
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-17 09:57:55 -0300

    Adicionado CSS a pagina de login

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { BrowserRouter } from 'react-router-dom';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import './login.css';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <section>
-          <Header />
-          <Content />
-          <Footer />
-        </section>
+        <div className="content-total">
+          <div className="header">
+            <Header />
+          </div>
+          <div className="content">
+            <Content />
+          </div>
+          <div className="footer">
+            <Footer />
+          </div>
+        </div>
       </BrowserRouter>
     );
   }
