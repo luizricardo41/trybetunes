e25270a04b826f209d181c035339d5c560c35f02
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-19 15:51:37 -0300

    Resoluçao do requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,62 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      buttonEnable: true,
+      // name: '',
+    });
+    this.handleChange = this.handleChange.bind(this);
+    this.enableButton = this.enableButton.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.enableButton(value);
+  }
+
+  enableButton(value) {
+    const minValue = 2;
+    if (value.length >= minValue) {
+      this.setState({
+        buttonEnable: false,
+      });
+    } else {
+      this.setState({
+        buttonEnable: true,
+      });
+    }
+  }
+
   render() {
+    const {
+      state: {
+        buttonEnable,
+      },
+      handleChange,
+    } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        Page Search
+        <label htmlFor="name">
+          <input
+            type="text"
+            name="name"
+            onChange={ handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ buttonEnable }
+          >
+            Pesquisar
+          </button>
+        </label>
       </div>
     );
   }
