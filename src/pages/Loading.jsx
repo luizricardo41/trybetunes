a257ba254e2a84d6ad951b34936401c94e33e82c
Luiz Ricardo <luizricardo41@gmail.com>
2022-05-02 09:40:37 -0300

    fix: correção de arquivo;

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Loading extends Component {
   render() {
     return (
-      <p>Carregando...</p>
+      <> Carregando...</>
     );
   }
 }
