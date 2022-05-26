ce317b78ad6192ede057ce94a82c01619765720b
Luiz Ricardo <luizricardo41@gmail.com>
2022-05-26 20:18:55 -0300

    fix: movido arquivo index;

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './src/index.css';
+import App from './src/App';
 
 ReactDOM.render(
   <React.StrictMode>
